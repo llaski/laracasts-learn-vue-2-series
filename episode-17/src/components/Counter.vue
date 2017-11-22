642bc1e6becf7ec533d8c5019767afc1921eb480
Larry <larry.laski@gmail.com>
2017-11-22 16:11:55 -0500

    episode 17 - 20

---
+++ episode-17/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+      <h2>The count: {{ count }}</h2>
+
+      <p>
+        <button @click="count += 20">Increment</button>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  background: grey;
+  padding: 10px;
+  border: 1px solid red;
+  margin-bottom: 10px;
+}
+</style>
